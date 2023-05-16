e3074900e1e883775009c9b2112eacc6317e5f52
Quratulain <aquratulain98@gmail.com>
2023-05-16 23:24:22 +0500

    Updated

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ function removeval(){
     var x = document.getElementById('result').value;
     document.getElementById('result').value = document.getElementById('result').value.slice(0,x.length-1);
 }
+function fac(num){
+    let m = document.getElementById('result').value +=num;
+    // let n = document.getElementById('result').value.slice(0,m.length-1);
+    document.getElementById('result').value = factorial(document.getElementById('result').value.slice(0,m.length-1));
+}
+function factorial(n){
+    let i,f = 1;
+    for(i=n;i>=1;i--){
+        f = f*i;
+    }
+    return f;
+}
